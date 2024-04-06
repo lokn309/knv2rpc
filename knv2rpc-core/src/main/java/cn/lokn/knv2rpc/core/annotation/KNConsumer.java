27ceb43cd50bbbb14dc3b0ddffe9d04894563e31
lokn309 <1163141991@qq.com>
2024-04-06 16:46:05 +0800

    add consumer annotation

---
+++ knv2rpc-core/src/main/java/cn/lokn/knv2rpc/core/annotation/KNConsumer.java
@@ -0,0 +1,10 @@
+package cn.lokn.knv2rpc.core.annotation;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+public @interface KNConsumer {
+}
